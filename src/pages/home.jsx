e3477b019895639b7a4ae4b3eb642d0fa209b235
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-02-11 21:10:56 -0800

    Assignment 1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,21 +1,17 @@
 import './home.css';
 import { Link } from 'react-router-dom';
-import bg_store from '../assets/background.jpg';
 
 function Home() {
     return (
         <div className="home page">
-            <img src={bg_store} alt="" />
-
-            <div className="info">
-                <h4>Welcome to Organika Store</h4>
-                <h6>Fresh food, 1-click away!</h6>
+            <div className="info bg-lightdark text-white p-5 rounded-4 shadow-lg">
+                <h1 className="display-4 fw-bold">Welcome to Organika Store</h1>
+                <h4 className="fw-light">Fresh food, 1-click away!</h4>
             </div>
 
-            <div className="controls">
-                <Link to="/catalog" className="btn">Check Our Catalog!</Link>
+            <div className="controls mt-4">
+                <Link to="/catalog" className="btn btn-success btn-lg px-5 py-3 fw-bold shadow">Check Our Catalog!</Link>
             </div>
-
         </div>
     );
 }
