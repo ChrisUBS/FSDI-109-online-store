c3368f081f9458d20a3183c61883bbec05a0f91d
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2025-02-14 16:27:20 -0800

    Class 2

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,6 +1,66 @@
 import './admin.css';
+import { useState } from 'react';
 
 function Admin() {
+
+    const [allCoupons, setAllCoupons] = useState([]);
+    const [allProducts, setAllProducts] = useState([]);
+
+    const [coupon, setCoupon] = useState({
+        code: '',
+        discount: 0
+    });
+
+    const [product, setProduct] = useState({
+        title: '',
+        price: 0,
+        image: '',
+        category: ''
+    });
+
+    function handleProduct(e) {
+        const text = e.target.value;
+        const name = e.target.name;
+        console.log('text changed!', text, name);
+
+        let copy = { ...product };
+        copy[name] = text;
+        setProduct(copy);
+    }
+
+    function saveProduct() {
+        console.log('Product:', product);
+        let copy = [...allProducts];
+        copy.push(product);
+        setAllProducts(copy);
+        console.log('All Products:', allProducts);
+    }
+
+    function handleCoupon(e) {
+        const text = e.target.value;
+        const name = e.target.name;
+        console.log('text changed!', text, name);
+
+        /**
+         * 3 steps to modify Obj or Arrays in State vars:
+         * - Create a copy
+         * - Modify the copy
+         * - Set the copy back
+         */
+
+        let copy = { ...coupon };
+        copy[name] = text;
+        setCoupon(copy);
+    }
+
+    function saveCoupon() {
+        console.log('Coupon:', coupon);
+        let copy = [...allCoupons];
+        copy.push(coupon);
+        setAllCoupons(copy);
+        console.log('All Coupons:', allCoupons);
+    }
+
     return (
         <div className="admin page">
             <h1>Store administration</h1>
@@ -11,25 +71,27 @@ function Admin() {
                     <form>
                         <div className="form-group">
                             <label htmlFor="product-name">Product name</label>
-                            <input type="text" className="form-control" id="product-name" placeholder="Enter product name" />
+                            <input type="text" className="form-control" id="product-name" placeholder="Enter product name" onBlur={handleProduct} name='title'/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="product-price">Product price</label>
-                            <input type="number" className="form-control" id="product-price" placeholder="Enter product price" />
+                            <input type="number" className="form-control" id="product-price" placeholder="Enter product price" onBlur={handleProduct} name='price'/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="product-image">Product image</label>
-                            <input type="text" className="form-control" id="product-image" placeholder="Enter product image" />
+                            <input type="text" className="form-control" id="product-image" placeholder="Enter product image" onBlur={handleProduct} name='image'/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="product-category">Product category</label>
-                            <input type="text" className="form-control" id="product-category" placeholder="Enter product category" />
+                            <input type="text" className="form-control" id="product-category" placeholder="Enter product category" onBlur={handleProduct} name='category'/>
                         </div>
 
-                        <div className='justify-content-center d-flex'>
-                            <button type="submit" className="btn btn-dark">Save Product</button>
-                        </div>
                     </form>
+                        <div className='justify-content-center d-flex'>
+                            <button className="btn btn-dark" onClick={saveProduct}>Save Product</button>
+                        </div>
+
+                        {allProducts.map(pr => <li>{pr.title} - ${pr.price} - {pr.category}</li>)}
                 </div>
 
                 <div className='coupons col-4'>
@@ -38,18 +100,21 @@ function Admin() {
                     <form>
                         <div className="form-group">
                             <label htmlFor="code">Code</label>
-                            <input type="text" className="form-control" id="code" placeholder="Enter code" />
+                            <input type="text" className="form-control" id="code" placeholder="Enter code" onBlur={handleCoupon} name='code'/>
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="discount">Discount</label>
-                            <input type="number" className="form-control" id="discount" placeholder="Enter discount" />
+                            <input type="number" className="form-control" id="discount" placeholder="Enter discount" onBlur={handleCoupon} name='discount'/>
                         </div>
 
-                        <div className='justify-content-center d-flex'>
-                            <button type="submit" className="btn btn-dark">Save Coupon</button>
-                        </div>
                     </form>
+                        <div className='justify-content-center d-flex'>
+                            <button className="btn btn-dark" onClick={saveCoupon}>Save Coupon</button>
+                        </div>
+
+                        {allCoupons.map(cp => <li>{cp.code} - {cp.discount}%</li>)}
+
                 </div>
             </div>
         </div>
@@ -57,3 +122,9 @@ function Admin() {
 }
 
 export default Admin;
+
+/**
+ * on click of the Save Coupon bn
+ * call a saveCoupon fn
+ * the fn should console log the coupon variable
+ */
